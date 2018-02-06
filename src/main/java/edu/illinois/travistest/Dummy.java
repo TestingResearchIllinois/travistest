cdc1837afd0cbf46a89144d7f0ba9a1d2f3641f1
Peiyuan Zhao <zpy1995@gmail.com>
2018-02-05 18:58:29 -0600

    e6309db42d7e61d8a7b53005c58dde0dbd785734 replay gib_eks

--- src/main/java/edu/illinois/travistest/Dummy.java
+++ src/main/java/edu/illinois/travistest/Dummy.java
@@ -2,5 +2,10 @@ package edu.illinois.travistest;
 
 public class Dummy {
     public static void foo() {
+        System.out.println("FOO");
+    }
+
+    public static int bar() {
+        return 0;
     }
 }
